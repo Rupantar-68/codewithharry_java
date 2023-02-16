79b43087994ba74c1dd1fe8f251a84295992d198
Rupantar-68 <chakrabortyr2002@gmail.com>
2023-02-16 12:44:53 +0530

    commit

--- src/ex6.java
+++ src/ex6.java
@@ -1,2 +1,111 @@
+import java.util.Arrays;
+
 public class ex6 {
+    public static void main(String[] args) {
+        // q1)sum of 5 floats
+//        float[] arr={22.2f,33.3f,44.4f,55.5f,66.6f};
+//        float sum =0;
+//        for (int i = 0; i < arr.length; i++) {
+//          sum = sum +arr[i];
+//        }
+//        System.out.println(sum);
+
+        //q2) whether given integer is present in array or not
+//        int target = 9;
+//        int[] arr ={1,6,5,9,4,7};
+//
+//        for (int i = 0; i < arr.length; i++) {
+//            if(arr[i]==target){
+//                System.out.println("Found at : "+i);
+//            }
+//        }
+
+        //q3) Calculate an average marks from an array containing marks of all students in physics using for-each loop
+//
+
+        //q4) addition of two matrices of size 2x3
+
+//        int [][] matrix1 ={{2,3,4},{5,6,7}};
+//        int [][] matrix2 ={{3,7,4},{4,6,9}};
+//
+//        for (int i = 0; i < matrix1.length; i++) {
+//            for (int j = 0; j < matrix1[i].length; j++) {
+//                System.out.print(matrix1[i][j]+" ");
+//            }
+//            System.out.println();
+//        }
+//        System.out.println("Added with: ");
+//
+//        for (int i = 0; i < matrix2.length; i++) {
+//            for (int j = 0; j < matrix2[i].length; j++) {
+//                System.out.print(matrix2[i][j]+" ");
+//            }
+//            System.out.println();
+//        }
+//        //addition
+//        int[][] result= {{0,0,0},{0,0,0}};
+//        for (int i = 0; i < matrix1.length; i++) {
+//            for (int j = 0; j < matrix1[i].length; j++) {
+//                result[i][j] = matrix1[i][j]+matrix2[i][j];
+//            }
+//        }
+//        System.out.println("Gives answer: ");
+//        //printing result
+//        for (int i = 0; i < result.length; i++) {
+//            for (int j = 0; j < result[i].length; j++) {
+//                System.out.print(result[i][j]+" ");
+//            }
+//            System.out.println("");
+//        }
+
+        //q5) array reverse
+//        int[] arr={12,99,77,54,33,88};
+//        Arrays.sort(arr);
+//        System.out.println(Arrays.toString(arr));
+//
+//        for (int i = arr.length-1; i >=0 ; i--) {
+//            System.out.print(arr[i]+" ");
+//        }
+
+        //q6) maximum element in an array
+//        int[] arr ={111,99,33,44,100};
+//        int max = 0;
+//        for (int i = 0; i < arr.length; i++) {
+//            if (arr[i]>max){
+//                max = arr[i];
+//            }
+//        }
+//        System.out.println(max);
+
+        //q7) minimum element in a jav array
+//        int[] arr = {111,99,33,44,100};
+//        int min =arr[0];
+//        for (int i = 0; i < arr.length; i++) {
+//            if (arr[i]<min){
+//                min = arr[i];
+//            }
+//        }
+//        System.out.println("Minimum element is : "+ min);
+
+        //q8) whether it is sorted or not
+//        int [] arr2 ={55,66,77,88};
+//        boolean isSorted = true;
+//
+//        for (int i = 0; i < arr2.length-1; i++) {
+//            if (arr2[i] > arr2[i + 1]) {
+//                isSorted = false;
+//                break;
+//
+//            }
+//        }
+//        if (isSorted){
+//            System.out.println("Sorted array!");
+//        }
+//        else {
+//            System.out.println("Unsorted array!");
+//        }
+
+        
+
+    }
 }
